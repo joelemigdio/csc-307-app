e7a4eade30fc10d9ba55eb8c5138fbf82afde977
joelemigdio <156865995+joelemigdio@users.noreply.github.com>
2026-01-15 00:08:01 -0800

    steps 3 and 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,6 +5,40 @@ const port = 8000;
 
 app.use(express.json());
 
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -13,4 +47,5 @@ app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
-});
+}
+);
